ffa35575eaad74b4cd8421f9da2afdce07d5b9b5
emodatt08 <shillary@itconsortiumgh.com>
2018-01-26 15:18:09 +0000

    added the image library Pillow

---
+++ images/pillow.py
@@ -0,0 +1,27 @@
+from PIL import Image as image
+
+img  = image.open("/Users/emodatt08/Desktop/flip2.png")
+img2 = image.open("/Users/emodatt08/Desktop/error.png")
+
+#get image size and format
+# print img.size
+# print img.format
+
+#open image in default image program
+# img.show()
+
+###CROP IMAGE
+# give area of image
+# area = (20, 100, 200, 200)
+# first two parameters are the x/y coordinates of the right corner and other two are the left corner respectively
+# crop_image = img.crop(area)
+#show cropped image
+# crop_image.show()
+
+
+###COMBINE TWO IMAGES
+# give area of image
+# area = (400,700)
+# combine image by passing two parameters, first parameter takes in the image and the second the area
+# img.paste(img2, area)
+# img.show()
