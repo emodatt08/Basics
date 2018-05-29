832e8923b55d6fd25e1fa5f0570c3670c79fa6f4
emodatt08 <shillary@itconsortiumgh.com>
2018-05-29 10:41:37 +0000

    added an sftp script

--- sftp/send.py
+++ sftp/send.py
@@ -17,7 +17,7 @@ class SFTP:
         try:
             send = sf.Connection(host=self.url, username = self.username, password = self.password)
             remotePath = '/var/www/html/'+ str(datetime.now().year) + '/' + str(datetime.now().strftime('%B')) + '/' + 'flip2.png'
-            localPath = '/Users/youruseraccount/Desktop/flip2.png'
+            localPath = '/Users/emodatt08/Desktop/flip2.png'
             send.put(localPath, remotePath)
 
             send.close()
@@ -27,8 +27,8 @@ class SFTP:
 
 #pprint('/var/www/html/'+ str(datetime.now().year) + '/' + str(datetime.now().strftime('%B')) + '/' + 'flip2.png')
 #set remote server Url
-remoteUrl = ""
+remoteUrl = "156.0.235.14"
 #Initialize class with server credentials
-sftp = SFTP(remoteUrl, "", "")
+sftp = SFTP(remoteUrl, "fone", "KwameIsaBoy19")
 #send file to remote server
 sftp.sendFile()
