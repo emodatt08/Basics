6899820f9dd40ef5bf132b86794fceb8e4dfc805
emodatt08 <shillary@itconsortiumgh.com>
2018-05-29 10:16:51 +0000

    added an sftp script

---
+++ sftp/send.py
@@ -0,0 +1,34 @@
+import pysftp as sf
+from pprint import pprint
+from datetime import datetime
+
+
+class SFTP:
+    
+
+    """ Initialize global variables """
+    def __init__(self, url, username, password):
+        self.username = username
+        self.password = password
+        self.url = url
+
+    """ SFTP function """
+    def sendFile(self):
+        try:
+            send = sf.Connection(host=self.url, username = self.username, password = self.password)
+            remotePath = '/var/www/html/'+ str(datetime.now().year) + '/' + str(datetime.now().strftime('%B')) + '/' + 'flip2.png'
+            localPath = '/Users/youruseraccount/Desktop/flip2.png'
+            send.put(localPath, remotePath)
+
+            send.close()
+        
+        except Exception, e:
+            print str(e)
+
+#pprint('/var/www/html/'+ str(datetime.now().year) + '/' + str(datetime.now().strftime('%B')) + '/' + 'flip2.png')
+#set remote server Url
+remoteUrl = ""
+#Initialize class with server credentials
+sftp = SFTP(remoteUrl, "", "")
+#send file to remote server
+sftp.sendFile()
